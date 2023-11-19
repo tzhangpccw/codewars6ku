da54745e5bfc745dbb89b99759b47c25ea256892
tzhangpccw <107375290+tzhangpccw@users.noreply.github.com>
2023-11-19 14:21:49 +1000

    Update Convert string to camel case.js

--- Convert string to camel case.js
+++ Convert string to camel case.js
@@ -1,4 +1,4 @@
-/* 
+/* 19112023
 Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
 
 Examples

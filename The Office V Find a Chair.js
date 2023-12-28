bfab3fca40f3d6fc88313f35079dd29939f14a63
tzhangpccw <107375290+tzhangpccw@users.noreply.github.com>
2023-12-29 08:44:27 +1000

    Update The Office V Find a Chair.js

--- The Office V Find a Chair.js
+++ The Office V Find a Chair.js
@@ -37,3 +37,20 @@ function meeting(x, need) {
 
   return res;
 }
+
+
+function meeting(rooms, need) {
+  if (need <= 0) {
+    return 'Game On';
+  }
+  const taken = [];
+  for (const [{ length: chairs }, people] of rooms) {
+    const take = Math.min(Math.max(people - chairs, 0), need);
+    taken.push(take)
+    need -= take;
+    if (need <= 0) {
+      return taken;
+    }
+  }
+  return 'Not enough!';
+}
